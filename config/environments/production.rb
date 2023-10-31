411a5e35591415650b59bac97cf234c4fc9a8440
ds998 <danilo.stefanovic6@gmail.com>
2023-10-31 18:40:24 +0100

    Added exec files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Official::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 end
